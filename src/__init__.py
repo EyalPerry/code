1ed3293a593359569efbfe8f3a51479ab80b8d3a
Eyal Perry <e.perry.pa@gmail.com>
2025-03-29 17:39:46 +0300

    use code

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,16 @@
 from datetime import datetime, date
-from pyspark.sql import Row
+
 from pyspark.sql import SparkSession
 
-if __name__ == "__main__":
-    spark = SparkSession.builder.getOrCreate()
+from delta import *
 
-    df = spark.createDataFrame([
-        Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
-        Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
-        Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
-    ])
-    df.show()
+def init_spark():
+    builder = SparkSession.builder \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    return spark
+
+if __name__ == "__main__":
+    pass
