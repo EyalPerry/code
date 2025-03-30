d0864250cd82dc8c89a0d84632bc987dbeb46ab8
Eyal Perry <e.perry.pa@gmail.com>
2025-03-30 11:40:26 +0300

    spark delta and kafka

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,19 @@
 from pyspark.sql import SparkSession
 
-from delta import *
 
 def init_spark() -> SparkSession:
+    from delta import configure_spark_with_delta_pip
+
+    # Set packages needed at runtime, including both Delta and Kafka
+    extra_packages = [
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0"
+    ]
+    
     builder = SparkSession.builder \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
 
-    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    spark = configure_spark_with_delta_pip(builder, extra_packages).getOrCreate()
     return spark
 
 if __name__ == "__main__":
